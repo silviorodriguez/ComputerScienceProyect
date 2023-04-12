77d77135ecc39a5a9b24b018a6516e2d45fe55a3
silviorodriguez <118927826+silviorodriguez@users.noreply.github.com>
2023-04-11 21:02:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ let estudiantes = [
         "materias": "Programacion, HTML, Css",
         "calificacion": 4.5,
         "foto": "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "profesor": "Enrique Maya",
-        "curso": "Javascript"
+        "profesor": "Enrique Maya"
+        
     },
     {
         "documento": 107090062,
@@ -30,8 +30,8 @@ let estudiantes = [
         "materias": " HTML, Css",
         "calificacion": 3.5,
         "foto": "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "profesor": "Roberto Romero",
-        "curso": "Javascript"
+        "profesor": "Roberto Romero"
+        
     },
     {
         "documento": 123456789,
@@ -41,8 +41,8 @@ let estudiantes = [
         "materias": "Programacion",
         "calificacion": 4.8,
         "foto": "https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "profesor": "Roberto Romero",
-        "curso": "Javascript"
+        "profesor": "Roberto Romero"
+       
 
     },
 
@@ -54,8 +54,8 @@ let estudiantes = [
       "materias": "Programacion, HTML, Css",
       "calificacion": 2.5,
       "foto": "https://images.pexels.com/photos/2284233/pexels-photo-2284233.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-      "profesor": "Enrique Maya",
-      "curso": "Javascript"
+      "profesor": "Enrique Maya"
+      
   },
   {
       "documento": 107090062,
@@ -65,8 +65,8 @@ let estudiantes = [
       "materias": " HTML, Css",
       "calificacion": 3.5,
       "foto": "https://images.pexels.com/photos/4926674/pexels-photo-4926674.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      "profesor": "Roberto Romero",
-      "curso": "Javascript"
+      "profesor": "Roberto Romero"
+    
   },
   {
       "documento": 123456789,
@@ -75,48 +75,177 @@ let estudiantes = [
       "edad": 7,
       "materias": "Programacion",
       "calificacion": 4.8,
-      "foto": "https://images.pexels.com/photos/2530775/pexels-photo-2530775.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      "profesor": "Enrique Maya",
-      "curso": "Javascript"
+      "foto": "https://images.pexels.com/photos/5119214/pexels-photo-5119214.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "profesor": "Enrique Maya"
+     
 
   },
+  {
+    "documento": 321564681,
+    "nombre": "Carlos",
+    "apellido": "Suarez",
+    "edad": 28,
+    "materias": "Programacion, Javascript, Css",
+    "calificacion": 2.8,
+    "foto": "https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "profesor": "Enrique Maya"
+
+
+},
+{
+  "documento": 6584351654,
+  "nombre": "Esther",
+  "apellido": "Chacon",
+  "edad": 67,
+  "materias": "Fundamentos de programacion",
+  "calificacion": 1.8,
+  "foto": "https://images.pexels.com/photos/2530775/pexels-photo-2530775.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "profesor": "Enrique Maya"
+
+
+},
+{
+  "documento": 123456789,
+  "nombre": "Jhojan ",
+  "apellido": "Torres",
+  "edad": 18,
+  "materias": "Html, Css",
+  "calificacion": 3.0,
+  "foto": "https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "profesor": "Roberto Romero"
+ 
+
+},
+{
+  "documento": 3541431554,
+  "nombre": "Maria",
+  "apellido": "Lopez",
+  "edad": 25,
+  "materias": "Fundamentos de la Programacion",
+  "calificacion": 5.0,
+  "foto": "https://images.pexels.com/photos/2083751/pexels-photo-2083751.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "profesor": "Enrique Maya"
+
+
+},
+{
+  "documento": 123456789,
+  "nombre": "Carolina",
+  "apellido": "Romo",
+  "edad": 35,
+  "materias": "Javascript",
+  "calificacion": 4.8,
+  "foto": "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "profesor": "Roberto Romero"
+  
+
+},
+{
+  "documento": 123456789,
+  "nombre": "Esteban",
+  "apellido": "Ramirez",
+  "edad": 36,
+  "materias": "Css, fundamentos de la Programacion",
+  "calificacion": 2.3,
+  "foto": "https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "profesor": "Enrique Maya"
+ 
+
+},
 ]
 
 
-document.getElementById("formulario").addEventListener("submit", buscar);
+document.getElementById("formulario").addEventListener("submit", buscar,);
+document.getElementById("boton").addEventListener("click",gradoA);
+document.getElementById("boton2").addEventListener("click",gradoB);
+document.getElementById("busqueda").addEventListener("keyup",buscar);
+
+function gradoA(evt){
+  evt.preventDefault();
+  const auxa= estudiantes.filter((estudiante)=>{
+        return estudiante.profesor == "Enrique Maya"
+  });
+  dibujarSecuencial(auxa);
+}
+
+function gradoB(evt){
+  evt.preventDefault();
+  const auxa= estudiantes.filter((estudiante)=>{
+        return estudiante.profesor == "Roberto Romero"
+  });
+  dibujarSecuencial(auxa);
+}
 
 function buscar(evt){
   evt.preventDefault();
-  let datoBarra = document.getElementById("busqueda").value;
+  let cadenaBuscar = document.getElementById("busqueda").value;
 
-  estudiantes.filter((estudiante) =>{
-    return estudiantes.nombre === datoBarra
+
+ const aux = estudiantes.filter((estudiante)=>{
+    return estudiante.nombre.toLowerCase().includes(cadenaBuscar.toLowerCase())
   });
+  
+  dibujarSecuencial(aux);
 
-  dibujar(datoBarra);
+
+
+
+}
+  
+function dibujarSecuencial(estudiantesFilter){
+  document.querySelector("#contenedor").innerHTML ="";
+  estudiantesFilter.forEach((estudiante, i)=>{
+    document.querySelector("#contenedor").innerHTML +=`<div class="card" style="width: 18rem;">
+    <img src="${estudiante.foto}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${estudiante.nombre} ${estudiante.apellido}</h5>
+      <p class="card-text">${estudiante.materias}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Calificacion: ${estudiante.calificacion} </li>
+      <li class="list-group-item">Edad: ${estudiante.edad}</li>
+      <li class="list-group-item">Curso: ${estudiante.profesor}</li>
+      <li class="list-group-item">Curso: ${estudiante.curso}</li>
+    </ul>
+    <div class="card-body">
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+    </div>`
+  })
 }
 
-function dibujar(nombre){
-  document.querySelector("#contenedor").innerHTML = "";
-  estudiantes.forEach((estudiante, i)=>{   
-  document.querySelector("#contenedor").innerHTML += `<div class="card" style="width: 18rem;">
+dibujarSecuencial(estudiantes);
+
+
+
+function guardarDatos(){
+  let datos1 = document.getElementById("inputDocumento").value
+  let datos2 =document.getElementById("inputNombre").value
+  let datos3 =document.getElementById("inputApellido").value
+  let datos4 =document.getElementById("inputEdad").value
+  let datos5 =document.getElementById("inputMaterias").value
+  let datos6 =document.getElementById("inputcalificacion").value
+  let datos7 =document.getElementById("inputFoto").value
+  let datos8 =document.getElementById("inputProfesor").value
+  let datos9 =document.getElementById("inputCurso").value
   
-  <img src="${estudiante.foto}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${estudiante.nombre} ${estudiante.apellido}</h5>
-    <p class="card-text">${estudiante.materias}</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Calificacion: ${estudiante.calificacion} </li>
-    <li class="list-group-item">Edad: ${estudiante.edad}</li>
-    <li class="list-group-item">Curso: ${estudiante.profesor}</li>
-    <li class="list-group-item">Curso: ${estudiante.curso}</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  </div>`
-  })
+  estudiantes.push({"documentos":datos1,"nombre":datos2,"apellidos":datos3, "edad":datos4, "materias":datos5, "calificacion":datos6, "foto":datos7, "profesor":datos8, "curso":datos9})
+  console.log(estudiantes)
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
